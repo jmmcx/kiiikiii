49e6faf821a666e572f7d4c59821c580f90a558e
Patramon <132761777+Patramon@users.noreply.github.com>
2025-03-24 14:34:11 +0700

    Update to connect to phpMyAdmin

--- src/main/java/bean/dBConnection.java
+++ src/main/java/bean/dBConnection.java
@@ -1,3 +1,5 @@
+/* For connecting to DBeaver (Local Database) */
+/*
 package bean;
 
 import java.sql.Connection;
@@ -24,4 +26,34 @@ public class dBConnection {
             logger.error("Error shutting down MySQL cleanup thread: " + e.getMessage());
         }
     }
-}
+} */
+/* **For connecting to phpMyAdmin ** */
+package bean;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import util.ConfigUtil;
+
+public class dBConnection {
+    private static final Logger logger = LogManager.getLogger(dBConnection.class);
+    
+    public static Connection getConnection() throws Exception {
+        String driver = ConfigUtil.getDBConfig("db.driver");
+        String url = ConfigUtil.getDBConfig("db.url");
+        String user = ConfigUtil.getDBConfig("db.user");
+        String password = ConfigUtil.getDBConfig("db.password");
+        
+        Class.forName(driver);
+        return DriverManager.getConnection(url, user, password);
+    }
+    
+    public static void shutdown() {
+        try {
+            com.mysql.cj.jdbc.AbandonedConnectionCleanupThread.checkedShutdown();
+        } catch (Exception e) {
+            logger.error("Error shutting down MySQL cleanup thread: " + e.getMessage());
+        }
+    }
+} 
